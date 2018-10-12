f87761175046dc634bc514aca7dc38df69a2949f
Nikolay Lubomirov <origamista99@gmail.com>
2018-10-12 10:46:16 +0300

    Make new level of abstraction it the architecture in Services, trough
    adding abstract class BaseService and IService interface

---
+++ Cardstone/Cardstone.Services/Contracts/General/BaseService.cs
@@ -0,0 +1,28 @@
+﻿using Cardstone.Data.Context;
+using System.Collections.Generic;
+
+namespace Cardstone.Services.Contracts
+{
+    public abstract class BaseService : IService
+    {
+        public BaseService(ICardstoneContext context)
+        {
+            this.Context = context;
+        }
+
+        public BaseService(ICardstoneContext context, params IService[] services)
+            : this (context)
+        {
+            this.Services = new List<IService>();
+
+            foreach (IService service in services)
+            {
+                this.Services.Add(service);
+            }
+        }
+
+        public ICardstoneContext Context { get; }
+
+        public ICollection<IService> Services { get; }
+    }
+}
---
+++ Cardstone/Cardstone.Services/Contracts/General/IService.cs
@@ -0,0 +1,12 @@
+﻿using Cardstone.Data.Context;
+using System.Collections.Generic;
+
+namespace Cardstone.Services.Contracts
+{
+    public interface IService
+    {
+        ICardstoneContext Context { get; }
+
+        ICollection<IService> Services { get; }
+    }
+}
