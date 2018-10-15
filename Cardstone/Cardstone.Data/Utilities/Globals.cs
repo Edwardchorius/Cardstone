af4417d04444612bea69a3d701a375a8c34100c5
Nikolay Lubomirov <origamista99@gmail.com>
2018-10-16 02:47:13 +0300

    Global constants added

---
+++ Cardstone/Cardstone.Data/Utilities/Globals.cs
@@ -0,0 +1,9 @@
+﻿namespace Cardstone.Data.Utilities
+{
+    public static class Globals
+    {
+        public static int MIN_CARD_NAME_LENGTH = 2;
+
+        public static int MAX_CARD_NAME_LENGTH = 30;
+    }
+}
