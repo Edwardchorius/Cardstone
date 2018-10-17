7fd567c24106e963d8c89baf6ee15e814566c5d6
Edwardchorius <edwardcho96@abv.bg>
2018-10-17 07:45:29 +0300

    Final Updates on Tests

---
+++ Cardstone/Cardstone.Test/Services/Mock/PlayerMock.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cardstone.Test.Services.Mock
+{
+    internal class PlayerMock
+    {
+        private string username;
+        
+        public PlayerMock(string username)
+        {
+
+        }
+
+        public string Username
+        {
+            get
+            {
+                return this.username;
+            }
+            set
+            {
+                this.username = value;
+            }
+        }
+
+        public void GetPlayer(string username)
+        {
+            if (username == null)
+            {
+                throw new ArgumentNullException("Invalid value!");
+            }
+        }
+    }
+}
---
+++ Cardstone/Cardstone.Test/Services/PlayerServiceTest/GetPlayer_Should.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cardstone.Data.Context;
+using Cardstone.Data.Models;
+using Cardstone.Services;
+using Cardstone.Test.Services.Mock;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Cardstone.Test.Services.PlayerServiceTest
+{
+    [TestClass]
+
+    public class GetPlayer_Should
+    {
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenPassedNullValue()
+        {
+            //Arrange
+            string invalidUser = null;
+
+            var playerMock = new PlayerMock(invalidUser);
+
+
+            //Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() =>
+            playerMock.GetPlayer(invalidUser));
+        }
+
+        [TestMethod]
+        [DataRow("Blitzkrank")]
+        [DataRow("Steven")]
+        [DataRow("Edo")]
+        public void Should_ReturnPlayer_WhenPassedValidUsername(string username)
+        {
+            //Arrange
+            var contextOptions = new DbContextOptionsBuilder<CardstoneContext>()
+                .UseInMemoryDatabase(databaseName: "Should_ReturnPlayer_WhenPassedValidUsername")
+                .Options;
+
+
+
+            //Assert
+            using (var assertContext = new CardstoneContext(contextOptions))
+            {
+                var sut = new PlayerService(assertContext);
+
+                Player player = sut.GetPlayer(username);
+
+                Assert.IsNotNull(player);
+            }
+        }
+    }
+}
