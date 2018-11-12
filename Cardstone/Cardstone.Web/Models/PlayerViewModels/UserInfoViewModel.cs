9dcdf85cdc9e39e7d8772a7c6f39714c3127acc8
Nikolay Lubomirov <origamista99@gmail.com>
2018-11-12 03:55:02 +0200

    Created PlayerViewModel in order to attempt pass data in Partial View

---
+++ Cardstone/Cardstone.Web/Models/PlayerViewModels/UserInfoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Cardstone.Web.Models.PlayerViewModels
+{
+    public class UserInfoViewModel
+    {
+        public string AvatarURL { get; set; }
+
+        public int Coins { get; set; }
+
+        public int XP { get; set; }
+    }
+}
